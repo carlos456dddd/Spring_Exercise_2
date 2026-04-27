0645b939c7ebf23f2a3a369b9cb87b89cfe7ed1b
CarlosMamaniAchinquipa <85691606+carlos456dddd@users.noreply.github.com>
2026-04-27 12:31:54 -0500

    feat: add task service

--- src/main/java/com/posexample/springexample/dto/taskResponse.java
+++ src/main/java/com/posexample/springexample/dto/taskResponse.java
@@ -5,8 +5,5 @@ public record taskResponse(Long id,
                            String title,
                            String description,
                            String status,
-                           String priority,
-                           userResponse userResponses,
-                           projectResponse projectResponses,
                            LocalDateTime created_at) {
 }
