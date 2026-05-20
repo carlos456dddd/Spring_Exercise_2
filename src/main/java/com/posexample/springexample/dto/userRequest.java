5ef4b989bfdcb4f4d01f25b7930abe79f6555257
CarlosMamaniAchinquipa <85691606+carlos456dddd@users.noreply.github.com>
2026-05-19 19:03:13 -0500

    fix: add builder anotation

--- src/main/java/com/posexample/springexample/dto/userRequest.java
+++ src/main/java/com/posexample/springexample/dto/userRequest.java
@@ -1,7 +1,10 @@
 package com.posexample.springexample.dto;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record userRequest(String username,
                           String email,
                           String password
--- src/main/java/com/posexample/springexample/dto/userResponse.java
+++ src/main/java/com/posexample/springexample/dto/userResponse.java
@@ -1,6 +1,9 @@
 package com.posexample.springexample.dto;
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record userResponse(Long Id,
                            String username,
                            String email,
